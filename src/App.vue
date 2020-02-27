39661ff626d571685981b147683db9c2d3302b6c
Sasha <sanaopan4ez@gmail.com>
2020-02-28 02:01:30 +0300

    feat: reproduce header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,84 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <Header :menuItems="menuItems"></Header>
 </template>
 
+<script>
+import Header from './components/Header';
+
+export default {
+  components: {
+    Header
+  },
+  data() {
+    return {
+      menuItems: [
+        { link: "#name", title: "Name" },
+        { link: "#e-mail", title: "E-mail" },
+        { link: "#summary", title: "Summary" },
+        { link: "#skills", title: "Skills" },
+        { link: "#code-examples", title: "Code Examples" },
+        { link: "#experience", title: "Experience" },
+        { link: "#education", title: "Education" },
+        { link: "#english", title: "English" }
+      ]
+    };
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: 'Montserrat';
+  src: url('./assets/fonts/Montserrat/Montserrat-Regular.ttf');
+  font-weight: 400;
 }
 
-#nav {
-  padding: 30px;
+@font-face {
+  font-family: 'Nunito';
+  src: url('./assets/fonts/Nunito/Nunito-Bold.ttf');
+  font-weight: 700;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html {
+  font-size: 10px;
+}
+
+body {
+  font-family: "Montserrat", "Arial", sans-serif;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+h1, h3 {
+  font-weight: bold;
+  font-family: "Nunito", "Arial", sans-serif;
+}
+
+h1 {
+  padding-top: 50px;
+  padding-bottom: 20px;
+  font-size: 50px;
+  color: hsl(132, 6%, 15%);
+  border-bottom: 1px solid hsl(132, 6%, 15%);
+}
+
+h3 {
+  font-size: 30px;
+  color: hsl(132, 6%, 15%);
+}
+
+.wrapper {
+  width: 1024px;
+  height: 100%;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <header class="header">
+    <div class="wrapper">
+      <nav class="header__navigation">
+        <ul class="menu">
+          <li
+            class="menu__item"
+            v-for="({link, title}) in menuItems"
+            :key="link"
+          >
+            <a :href="link">{{ title }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["menuItems"]
+};
+</script>
+
+<style lang="scss">
+.header {
+  position: fixed;
+  z-index: 1;
+  width: 100%;
+  background-color: hsl(173, 14%, 12%);
+}
+
+.menu {
+  display: flex;
+  list-style: none;
+
+  &__item {
+    flex-grow: 1;
+    font-weight: 400;
+    letter-spacing: 1.5px;
+    text-align: center;
+    color: hsl(180, 7%, 57%);
+
+    a {
+      display: block;
+      padding: 2vh 0;
+      font-size: 1.7rem;
+
+      &:hover {
+        transform: scale(1.1);
+        transition: 0.2s;
+      }
+    }
+
+    &:hover {
+      color: hsl(180, 31%, 82%);
+      background-color: hsl(173, 12%, 27%);
+      opacity: 0.9;
+    }
+
+    &:active {
+      color: hsl(0, 0%, 100%);
+    }
+  }
+}
+</style>
